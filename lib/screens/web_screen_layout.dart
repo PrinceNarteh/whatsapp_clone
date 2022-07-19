279a1ea229a153ed87f2dd73292c0b1d47a2901e
Prince Narteh <prinarteh@gmail.com>
2022-07-19 16:22:33 +0000

    adds assets

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contact_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Web Screen"),
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Column(
+            children: const [
+              // Web Profile Bar
+              // Web Search Bar
+              ContactList()
+            ],
+          )
+        ],
       ),
     );
   }

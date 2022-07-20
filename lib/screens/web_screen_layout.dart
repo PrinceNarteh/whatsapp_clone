c844b6f1b3f20084bf1d9d26077d4b456d81cc79
Prince Narteh <prinarteh@gmail.com>
2022-07-20 14:38:24 +0000

    add web profile app bar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/widgets/contact_list.dart';
+import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class WebScreenLayout extends StatelessWidget {
             child: SingleChildScrollView(
               child: Column(
                 children: const [
-                  // Web Profile Bar
+                  WebProfileBar(),
                   // Web Search Bar
                   ContactList()
                 ],
